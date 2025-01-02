eb865da2c1673f9a7a027150b40082b3f7df828b
Joseph Francis <hookedupjoe@users.noreply.github.com>
2025-01-02 11:33:46 -0500

    Updated posts to look like pages

--- single.php
+++ single.php
@@ -1,64 +1,35 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package pressinotheme
  */
 
-get_header();
-$tmpShown = false;
+ 
+ 	$themeShowSidebar = get_theme_mod( 'actappstd_show_sidebar' );
+	$tmpPageType = '';
+	if( $themeShowSidebar == false ){
+		$tmpPageType = 'full';
+	}
+	get_header();
+	ActAppTpl::showContentHeader($tmpPageType);
+
+	while ( have_posts() ){
+		the_post();
+		get_template_part( 'template-parts/content', 'page' );
+	}
+
+	ActAppTpl::showContenSidebar($tmpPageType);
+
+	ActAppTpl::showContenFooter($tmpPageType);
+	get_footer(); 
+
 ?>
 
-<div class="row">
-	<div class="col-sm-12 col-md-9 pad3">  
-		<div class="ui segment black">
-	
-
-	<main id="primary" class="site-main">
-
-		<?php
-		$tmpThisTitle = "";
-		while ( have_posts() ) :
-			the_post();
-			$tmpPostType = get_post_type_object( get_post_type($post) );
-			$tmpPostArchiveList = $tmpPostType->has_archive;
-			$tmpThisTitle = get_the_title();
-			echo ('<div class="ui header medium black">'.$tmpThisTitle.'</div>');
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			// // If comments are open or we have at least one comment, load up the comment template.
-			// if ( comments_open() || get_comments_number() ) :
-			// 	comments_template();
-			// endif;
-		endwhile; // End of the loop.
-		
-		?>
-
-	</main><!-- #main -->
-
-	
-	</div>
-	</div>  <?php // End Content ?>
-	<div class="col-sm-12 col-md-3 pad3">
-	
-	<div class="ui segment black">
-
-	<?php
-		
-		
-				
-				get_sidebar(); 
-			?>
-				
-	</div>
-	
-</div>
-
-</div>
-
-
-<?php
-
-get_footer();
